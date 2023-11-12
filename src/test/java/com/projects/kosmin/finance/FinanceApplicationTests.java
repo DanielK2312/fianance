fcc609608b31f5858f18553560e2efa01a6f8b1e
Daniel Kosmin <kosmindaniel@gmail.com>
2023-11-12 09:02:46 -0500

    initial commit

---
+++ src/test/java/com/projects/kosmin/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.kosmin.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
